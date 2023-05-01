c28631d171318b1014ee42f4bd1885e4433280ae
Hamza Rashid <hamzarashid720@gmail.com>
2023-05-01 13:06:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ if video_file and srt_file is not None:
     #with open(srt_file.name, "w") as srt_file_updated:
     #    srt_file_updated.write(srt_file.getvalue().decode('UTF-8'))
 
-    subs = SubtitlesClip(srt_file.name, generator)
+    subs = SubtitlesClip(srt_file.read(), generator)
     subtitles = subs.set_pos(('center','center'))
 
     video = VideoFileClip(video_file.name)
